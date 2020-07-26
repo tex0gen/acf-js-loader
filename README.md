373b9e8e85747371efe5c521b1e3357890480f10
Steve North <tex0gen@gmail.com>
2020-07-26 19:53:03 +0100

    Init

---
+++ README.md
@@ -0,0 +1,6 @@
+# ACF External Colour Picker Field
+This gets a list of colours and their values from a file in the theme directory.
+Each field that has a "background colour" field can have set colour values that match the theme design.
+The point of adding these colours in a single place within the theme directory is so that we don't need to update every background colour field that exists within the bootstrap-4-theme.
+
+This has not been developed for ACF v4, despite it being prepared.
---
+++ acf-js-loader.php
@@ -0,0 +1,90 @@
+<?php
+
+/*
+Plugin Name: Advanced Custom Fields: JS Loader
+Plugin URI: https://apexdevs.io/
+Description: Select optional javascript files to include per page.
+Version: 1.0.0
+Author: Tex0gen
+Author URI: https://apexdevs.io
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+// exit if accessed directly
+if( ! defined( 'ABSPATH' ) ) exit;
+
+
+// check if class already exists
+if( !class_exists('themestrap_ext_acf_field_js_loaders') ) :
+
+class themestrap_ext_acf_field_js_loaders {
+	
+	// vars
+	var $settings;
+	
+	
+	/*
+	*  __construct
+	*
+	*  This function will setup the class functionality
+	*
+	*  @type	function
+	*  @date	17/02/2016
+	*  @since	1.0.0
+	*
+	*  @param	void
+	*  @return	void
+	*/
+	
+	function __construct() {
+		
+		// settings
+		// - these will be passed into the field class.
+		$this->settings = array(
+			'version'	=> '1.0.0',
+			'url'		=> plugin_dir_url( __FILE__ ),
+			'path'		=> plugin_dir_path( __FILE__ )
+		);
+		
+		
+		// include field
+		add_action('acf/include_field_types', 	array($this, 'include_field')); // v5
+		add_action('acf/register_fields', 		array($this, 'include_field')); // v4
+	}
+	
+	
+	/*
+	*  include_field
+	*
+	*  This function will include the field type class
+	*
+	*  @type	function
+	*  @date	17/02/2016
+	*  @since	1.0.0
+	*
+	*  @param	$version (int) major ACF version. Defaults to 4
+	*  @return	void
+	*/
+	
+	function include_field( $version = 4 ) {
+		
+		// load textdomain
+		load_plugin_textdomain( 'TEXTDOMAIN', false, plugin_basename( dirname( __FILE__ ) ) . '/lang' ); 
+		
+		
+		// include
+		include_once( plugin_dir_path( __FILE__ ) . 'fields/class-js-loader-ext-v' . $version . '.php');
+	}
+	
+}
+
+
+// initialize
+new themestrap_ext_acf_field_js_loaders();
+
+
+// class_exists check
+endif;
+	
+?>
